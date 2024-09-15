8c4d59458dc46ddfd29afbb272d878da67e30210
NdiphiweQumba <71908219+NdiphiweQumba@users.noreply.github.com>
2024-09-15 04:09:46 +0200

    Update main.js
    Getting Current year

--- main.js
+++ main.js
@@ -3,3 +3,6 @@ let yr = document.querySelector(".years-exp");
 const d = new Date();
 let year = d.getFullYear();
 yr.textContent = year - 2021;
+
+let currentYear = document.querySelector(".currentYear");
+currentYear = d;
