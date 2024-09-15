33a9e4c1522c199509e0a5fb1aa504be6b252fef
NdiphiweQumba <71908219+NdiphiweQumba@users.noreply.github.com>
2024-09-15 04:12:37 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 // Years of experience // 
 let yr = document.querySelector(".years-exp");
 const d = new Date();
-let year = d.getFullYear();
-yr.textContent = year - 2021;
+let currentYear = d.getFullYear();
+yr.textContent = currentYear - 2021;
 
-let currentYear = document.querySelector(".currentYear");
-currentYear = d;
+// Current year. footer
+let footerYear = document.querySelector(".currentYear");
+footerYear.textContent = currentYear;
